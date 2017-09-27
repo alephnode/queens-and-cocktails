cb66cb362dedf0e89c187b00f24d7503bb737244
Stephen Ward <srmward@gmail.com>
2017-09-26 19:08:31 -0700

    removing ticket mentions

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
       <div className="navbar">
         <a href="#about" className="no-tiny">About</a>
         <a href="#bios" className="no-tiny">Bios</a>
-        <a href="#showtimes-tickets" className="no-tiny">Showtimes & Tickets</a>
+        <a href="#showtimes-tickets" className="no-tiny">Showtimes & Specials</a>
       </div>
     );
   }
--- src/components/showtimes-tickets/ShowtimesTickets.js
+++ src/components/showtimes-tickets/ShowtimesTickets.js
@@ -4,7 +4,7 @@ class ShowtimesTickets extends React.Component {
   render() {
     return (
       <div id="showtimes-tickets" className="showtimes-container">
-        <div className="showtimes-title">Upcoming Shows &amp; Tickets</div>
+        <div className="showtimes-title">Upcoming Shows &amp; Packages</div>
         <div className="showtimes-time-container">
           <div className="showtime">
             <p>Join us every Friday &amp; Saturday from 8:45 - 10:20 p.m.<br/></p>
